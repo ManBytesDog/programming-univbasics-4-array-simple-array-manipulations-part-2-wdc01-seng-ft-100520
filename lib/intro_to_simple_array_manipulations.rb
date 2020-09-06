3083f92b7bafcfd970c2210e70cad910073c6e8f
T.J. Marstiller <thomasj.marstiller@yahoo.com>
2020-09-06 20:51:49 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -2,6 +2,7 @@ def using_concat(array1, array2)
   array1.concat(array2)
 end
 
-def using_insert(array, element)
-  array.insert(array, element)
+def using_insert(index, element)
+  array = []
+  array.insert(index, element)
 end 
